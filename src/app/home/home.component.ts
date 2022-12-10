4a16acae4f8e70fe76a658b36b4e32fab4570b67
durgeshwagh <durgeshwagh3@gmail.com>
2022-12-11 02:07:17 +0530

    2nd commit

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -6,14 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit{
-  Form1 = document.getElementById("form1")
-  Form2 = document.getElementById("form2")
-  Form3 = document.getElementById("form3")
- 
-   Next1 = document.getElementById("Next1")
-   Next2 = document.getElementById("Next2")
 
-   Back2 = document.getElementById("Back2")
   constructor(){}
    step: any =1;
   ngOnInit(): void {
